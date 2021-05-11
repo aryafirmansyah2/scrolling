d83c1b28f06e3bc6705ce8e386df9dd5301f5c47
Naufaldi <naufaldi.rafif@gmail.com>
2021-05-11 09:20:06 +0700

    add team

--- components/Team/HeroTeam.js
+++ components/Team/HeroTeam.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import tw from 'twin.macro';
+import tw, { styled, css } from 'twin.macro';
 import Container from '../Common/Container';
 import Grid from '../Common/Grid';
+import HeadingOne from '../Typography/HeadingOne';
 
-const Section = tw.section`h-screen w-full bg-cream-500 relative z-10`;
-const MainContent = tw.div`col-span-full`;
+const Section = styled.section`
+  ${tw`sm:h-screen w-full bg-cream-500 relative z-10 flex`}
+  ${css`
+    height: 30vh;
+  `}
+`;
+const MainContent = tw.div`col-span-full h-full relative overflow-hidden`;
+const Title = tw(HeadingOne)`hidden sm:block text-brown-gold col-span-full text-center mt-40 sm:(mt-52) z-10 `;
 const HeroTeam = () => {
   return (
     <Section id="hero-team">
+      <img src="/assets/pattern/team/image-desktop.svg" tw="absolute object-cover w-full h-full" alt="" />
       <Container>
-        <Grid>
-          <MainContent></MainContent>
+        <Grid tw="h-full">
+          <MainContent>
+            <Title isWoodland>Our Team</Title>
+          </MainContent>
         </Grid>
       </Container>
     </Section>
--- components/Team/ListTeam.js
+++ components/Team/ListTeam.js
@@ -7,9 +7,10 @@ import HeadingTwo from '../Typography/HeadingTwo';
 import TextBody from '../Typography/TextBody';
 import listTeam from '@/data/listTeam';
 import Link from 'next/link';
+import HeadingOne from '../Typography/HeadingOne';
 
-const Section = tw.section`min-h-screen w-full bg-white relative z-10 py-20 sm:(pt-36 pb-40)`;
-const TextContent = tw.div`col-span-full col-start-1 sm:(col-span-10 col-start-2) mb-12`;
+const Section = tw.section`min-h-screen w-full bg-white relative z-10 pt-6 pb-20 sm:(pt-36 pb-40)`;
+const TextContent = tw.div`col-span-full col-start-1 flex flex-col sm:(col-span-10 col-start-2) mb-12`;
 const ItemTeam = tw.div`col-span-full lg:col-span-4 flex flex-col`;
 const ImgBg = tw.div`w-full aspect-h-1 bg-gray-500 mb-4 sm:mb-12`;
 const ImgTeam = tw.img`w-full h-full`;
@@ -19,15 +20,16 @@ const PositionTeam = tw(HeadingFour)`text-lg sm:text-2xl text-gray-200 mb-7 capi
 const QuoteTeam = tw(TextBody)`text-black mb-7 text-justify text-lg  md:text-2xl`;
 const SocialTeam = tw.div`flex w-full space-x-4`;
 const SocialLink = tw.a`font-semibold text-sm sm:text-base uppercase`;
+const Title = tw(HeadingOne)`text-brown-gold col-span-full mb-6 mt-4 text-center z-10 `;
 const ListTeam = () => {
   return (
     <Section id="list-team">
       <Container>
         <Grid tw="gap-y-14 sm:(gap-x-10 gap-y-24)">
           <TextContent>
+            <Title isWoodland>Our Team</Title>
             <TextBody isLgLight tw="text-center ">
-              We're the child of the 90's, transformed into mutants and interested in technological developments on
-              earth.
+              We're the child of the 90's, transformed into mutants and interested in technological developments on earth.
             </TextBody>
           </TextContent>
           {listTeam.map((listTeams, index) => (
